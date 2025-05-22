677efebf4b88d56ac3f7ecef39b57334608942aa
artezfit <kkamikadse@gmail.com>
2025-05-22 13:23:00 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    name: "Куриная грудка",
+    category: "Мясные",
+    image: "https://www.pngmart.com/files/5/Chicken-Breast-PNG-Transparent-Image.png",
+    proteins: 23,
+    fats: 2,
+    carbs: 0,
+    calories: 110
+  },
+  {
+    name: "Говядина",
+    category: "Мясные",
+    image: "https://www.pngmart.com/files/5/Raw-Beef-Steak-PNG-Image.png",
+    proteins: 20,
+    fats: 10,
+    carbs: 0,
+    calories: 170
+  },
+  {
+    name: "Филе трески",
+    category: "Рыба",
+    image: "https://www.pngmart.com/files/22/Fish-Meat-PNG-Isolated-Pic.png",
+    proteins: 18,
+    fats: 1,
+    carbs: 0,
+    calories: 82
+  },
+  {
+    name: "Сёмга",
+    category: "Рыба",
+    image: "https://www.pngmart.com/files/22/Salmon-Fish-PNG-File.png",
+    proteins: 20,
+    fats: 13,
+    carbs: 0,
+    calories: 208
+  },
+  {
+    name: "Творог 5%",
+    category: "Молочные",
+    image: "https://www.pngmart.com/files/21/Cottage-Cheese-PNG-Isolated-Photos.png",
+    proteins: 17,
+    fats: 5,
+    carbs: 3,
+    calories: 121
+  },
+  {
+    name: "Яйца куриные",
+    category: "Молочные",
+    image: "https://www.pngmart.com/files/1/Egg-PNG-Image.png",
+    proteins: 13,
+    fats: 11,
+    carbs: 1,
+    calories: 155
+  },
+  {
+    name: "Сыр Пармезан",
+    category: "Молочные",
+    image: "https://www.pngmart.com/files/21/Parmesan-Cheese-PNG-Image.png",
+    proteins: 35,
+    fats: 25,
+    carbs: 0,
+    calories: 392
+  }
+];
---
+++ script.js
@@ -0,0 +1,43 @@
+function createCard(product) {
+  return `
+    <div class="card">
+      <img src="${product.image}" alt="${product.name}">
+      <div class="card-body">
+        <h3>${product.name}</h3>
+        <p>Белки: ${product.proteins} г</p>
+        <p>Жиры: ${product.fats} г</p>
+        <p>Углеводы: ${product.carbs} г</p>
+        <p>Ккал: ${product.calories}</p>
+      </div>
+    </div>
+  `;
+}
+
+function renderProducts(products, category = "Все") {
+  const container = document.getElementById("productContainer");
+  container.innerHTML = "";
+  const filtered = category === "Все" ? products : products.filter(p => p.category === category);
+  filtered.forEach(product => {
+    container.innerHTML += createCard(product);
+  });
+}
+
+function populateCategories(products) {
+  const select = document.getElementById("categorySelect");
+  const categories = ["Все", ...new Set(products.map(p => p.category))];
+  categories.forEach(cat => {
+    const opt = document.createElement("option");
+    opt.value = cat;
+    opt.textContent = cat;
+    select.appendChild(opt);
+  });
+
+  select.addEventListener("change", () => {
+    renderProducts(products, select.value);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  populateCategories(products);
+  renderProducts(products);
+});
