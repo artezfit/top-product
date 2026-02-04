35b8dc0a937d604692c008d8bb01d0fe2e872193
artezfit <kkamikadse@gmail.com>
2026-02-04 22:29:02 +0300

    Update products list

--- products.js
+++ products.js
@@ -20,46 +20,26 @@ const products = [
     calories: 242
   },
   {
-    name: "Сыр Легкий 20% (Молочный мир)",
-    category: "Сыры",
-    image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/3223/02063223/norm/02063223.n_1.png",
-    description: "Очень классный сыр. Я его натираю на терке и храню уже в таком виде в холодильнике. Найти можно в Евроопте, Хите, Копеечке, Гиппо, Белмаркете",
-    proteins: 29,
-    fats: 9,
+    name: "Колбаса Никольская (Славянские продукты)",
+    category: "Мясные",
+    image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/7327/02117327/norm/02117327.n_2.png.webp",
+    description: "Очень классная колбаса, вот правда найти ее сложновато. Находил в Соседях и Гиппо",
+    proteins: 40,
+    fats: 5,
     carbs: 0,
-    calories: 197
-  },
+    calories: 204
+    },
+    {
+      name: "Колбаса Милано (Петруха)",
+      category: "Мясные",
+      image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/7629/00837629/norm/00837629.n_1.png",
+      description: "Найти можно почти в любом продуктовом",
+      proteins: 34,
+      fats: 10,
+      carbs: 0,
+      calories: 230
+      },
   {
-    name: "Сыр Легкий 35% (Брест-Литовск)",
-    category: "Сыры",
-    image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/1983/00761983/norm/00761983.n_1.png",
-    description: "Если нет 20% от Молочного мира, то берем этот, тоже топ за свои деньги. Найти можно почти в любом продуктовом",
-    proteins: 31.2,
-    fats: 18.2,
-    carbs: 0,
-    calories: 288.6
-  },
-  {
-name: "Колбаса Никольская (Славянские продукты)",
-category: "Мясные",
-image: "https://www.tablicakalorijnosti.ru/file/image/foodstuff/65f1a07ec5c146ceac546e0228a78566/8ce0381aed0448bc8dece6ec131367e2",
-description: "Очень классная колбаса, вот правда найти ее сложновато. Находил в Соседях и Гиппо",
-proteins: 40,
-fats: 5,
-carbs: 0,
-calories: 204
-},
-{
-name: "Колбаса Милано (Петруха)",
-category: "Мясные",
-image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/7629/00837629/norm/00837629.n_1.png",
-description: "Найти можно почти в любом продуктовом",
-proteins: 34,
-fats: 10,
-carbs: 0,
-calories: 230
-},
-{
 name: "Ветчина куриная (Natura)",
 category: "Мясные",
 image: "https://img.gippo.by/images/4810022026881.jpg",
@@ -70,25 +50,15 @@ carbs: 2,
 calories: 116
 },
 {
-name: "Говядина Кремлевская (Гродненский мясокомбинат)",
-category: "Мясные",
-image: "https://komvak.by/upload/iblock/d86/pv0wi10u1mjy3prve5j0i2pwoqq93n9z.png",
-description: "Ну оочень вкусная говяжья бастурма. Найти сложновато: видел в Грине иногда, но лично я покупаю в фирменном магазине Гродненского мясокомбината",
-proteins: 32,
-fats: 8,
-carbs: 0,
-calories: 200
-},
-{
-name: "Рулет Филейный (Бретский мясокомбинат)",
-category: "Мясные",
-image: "https://brestmeat.by/upload/resize_cache/webp/iblock/ae7/aew76vgxo500h3t1pbust3xaud5t9xxa.webp",
-description: "Альтернатива куриному филе. Находил только в Короне. Скорее всего еще можно найти в фирменном магазине Брестского мясокомбината",
-proteins: 18,
-fats: 3,
-carbs: 0,
-calories: 100
-},
+  name: "Рулет Филейный (Бретский мясокомбинат)",
+  category: "Мясные",
+  image: "https://brestmeat.by/upload/resize_cache/webp/iblock/ae7/aew76vgxo500h3t1pbust3xaud5t9xxa.webp",
+  description: "Альтернатива куриному филе. Находил только в Короне. Скорее всего еще можно найти в фирменном магазине Брестского мясокомбината",
+  proteins: 18,
+  fats: 3,
+  carbs: 0,
+  calories: 100
+  },
 {
 name: "Грудка Аппетитная (Ганна)",
 category: "Мясные",
@@ -120,6 +90,76 @@ carbs: 0,
 calories: 130
 },
 {
+  name: "Говядина Кремлевская (Гродненский мясокомбинат)",
+  category: "Мясные",
+  image: "https://komvak.by/upload/iblock/d86/pv0wi10u1mjy3prve5j0i2pwoqq93n9z.png",
+  description: "Ну оочень вкусная говяжья бастурма. Найти сложновато: видел в Грине иногда, но лично я покупаю в фирменном магазине Гродненского мясокомбината",
+  proteins: 32,
+  fats: 8,
+  carbs: 0,
+  calories: 200
+  },
+  {
+name: "Сосиски Мясные из индейки (Островецкое)",
+category: "Мясные",
+image: "https://io.activecloud.com/static-green-market/1400x1400-1493103-1705961029470.jpg?id=88821&version=1",
+description: "Найти можно в Грине, Гиппо и фирменном магазине Индюшонок",
+proteins: 10,
+fats: 5,
+carbs: 1,
+calories: 89
+},
+{
+name: "Пельмени Марьино с курицей",
+category: "Мясные",
+image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/0140/00850140/norm/00850140.n_1.png",
+description: "Одни из лучших пельменей на рынке. Найти можно в Соседях, Гиппо, Белмаркете, Грине, Короне",
+proteins: 12.1,
+fats: 3.4,
+carbs: 22,
+calories: 167
+},
+{
+name: "Пельмени Тимка (Добровский)",
+category: "Мясные",
+image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/0134/00720134/norm/00720134.n_1.png",
+description: "Очень вкусные пельмени. Найти можно (но не всегда) в Гиппо, Белмаркете, Грине, Короне",
+proteins: 13.1,
+fats: 5.2,
+carbs: 25.1,
+calories: 199.6
+},
+  {
+name: "Медальоны из тунца",
+category: "Мясные",
+image: "https://myikra.by/wp-content/uploads/2019/07/medalony-iz-tuncza.png",
+description: "Если захотелось рыбу, то это прям топ. Найти можно почти в любом продуктовом",
+proteins: 23,
+fats: 1,
+carbs: 0,
+calories: 108
+},
+  {
+    name: "Сыр Легкий 20% (Молочный мир)",
+    category: "Сыры",
+    image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/3223/02063223/norm/02063223.n_1.png",
+    description: "Очень классный сыр. Я его натираю на терке и храню уже в таком виде в холодильнике. Найти можно в Евроопте, Хите, Копеечке, Гиппо, Белмаркете",
+    proteins: 29,
+    fats: 9,
+    carbs: 0,
+    calories: 197
+  },
+  {
+    name: "Сыр Легкий 35% (Брест-Литовск)",
+    category: "Сыры",
+    image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/1983/00761983/norm/00761983.n_1.png",
+    description: "Если нет 20% от Молочного мира, то берем этот, тоже топ за свои деньги. Найти можно почти в любом продуктовом",
+    proteins: 31.2,
+    fats: 18.2,
+    carbs: 0,
+    calories: 288.6
+  },
+{
 name: "Мягкий творог 0,1% (Савушкин)",
 category: "Молочные",
 image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/6433/01796433/norm/01796433.n_1.png",
@@ -128,6 +168,16 @@ proteins: 10,
 fats: 0.1,
 carbs: 1,
 calories: 44.9
+},
+{
+name: "Сырок глазированный 5% (Молочная страна)",
+category: "Вкусняшки",
+image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/7204/02177204/norm/02177204.n_1.png",
+description: "Если любите сырки, то попробуйте эти и сможете съедать их на 1 больше, чем любые другие (за счет меньшего количества жира). Найти можно почти в любом продуктовом",
+proteins: 12.8,
+fats: 12.7,
+carbs: 33.2,
+calories: 300
 },
 {
 name: "Exponenta High-Pro",
@@ -218,56 +268,6 @@ proteins: 3,
 fats: 15,
 carbs: 3.5,
 calories: 200
-},
-{
-name: "Медальоны из тунца",
-category: "Мясные",
-image: "https://myikra.by/wp-content/uploads/2019/07/medalony-iz-tuncza.png",
-description: "Если захотелось рыбу, то это прям топ. Найти можно почти в любом продуктовом",
-proteins: 23,
-fats: 1,
-carbs: 0,
-calories: 108
-},
-{
-name: "Сосиски Мясные из индейки (Островецкое)",
-category: "Мясные",
-image: "https://io.activecloud.com/static-green-market/1400x1400-1493103-1705961029470.jpg?id=88821&version=1",
-description: "Найти можно в Грине, Гиппо и фирменном магазине Индюшонок",
-proteins: 10,
-fats: 5,
-carbs: 1,
-calories: 89
-},
-{
-name: "Сырок глазированный 5% (Молочная страна)",
-category: "Вкусняшки",
-image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/7204/02177204/norm/02177204.n_1.png",
-description: "Если любите сырки, то попробуйте эти и сможете съедать их на 1 больше, чем любые другие (за счет меньшего количества жира). Найти можно почти в любом продуктовом",
-proteins: 12.8,
-fats: 12.7,
-carbs: 33.2,
-calories: 300
-},
-{
-name: "Пельмени Марьино с курицей",
-category: "Мясные",
-image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/0140/00850140/norm/00850140.n_1.png",
-description: "Одни из лучших пельменей на рынке. Найти можно в Соседях, Гиппо, Белмаркете, Грине, Короне",
-proteins: 12.1,
-fats: 3.4,
-carbs: 22,
-calories: 167
-},
-{
-name: "Пельмени Тимка (Добровский)",
-category: "Мясные",
-image: "https://cdn.ime.by/UserFiles/images/catalog/Goods/0134/00720134/norm/00720134.n_1.png",
-description: "Очень вкусные пельмени. Найти можно (но не всегда) в Гиппо, Белмаркете, Грине, Короне",
-proteins: 13.1,
-fats: 5.2,
-carbs: 25.1,
-calories: 199.6
 },
 {
 name: "Хлеб белковый Славянка",
